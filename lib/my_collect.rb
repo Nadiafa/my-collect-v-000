2c1814c8a5c533bf45b846aa6cffa03bbdba573f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 12:56:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(arr)
   i = 0 
   collection = []
   while i < arr.length 
-    collection << yield(arr[i].upcase)
+    collection << yield(arr[i].split(" ").first)
     i+=1 
   end
   collection
